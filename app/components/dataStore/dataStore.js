ccf9c34e95cb42dc5b0922860d00a897963772c0
Himanshu <himanshunathani999@gmail.com>
2021-07-15 17:36:07 +0000

    dddd

--- app/components/dataStore/dataStore.js
+++ app/components/dataStore/dataStore.js
@@ -3,6 +3,27 @@ spa.$extend('dataStore', {
   member: {
     name: 'Name in [dataStore] component scope',
     currentPage: 1,
-  }
+  },
+
+  clients: {
+
+	0 :{
+	  name: 'Nike',
+	  thumbURL: 'asjdjasdj',
+	  password: '12345',
+	}, 
+
+	1 :{
+	  name: 'Puma',
+	  thumbURL: 'asjdjasdj',
+	  password: '12345',
+	}, 
+
+	2 : {
+	  name: 'Adidas',
+	  thumbURL: 'asjdjasdj',
+	  password: '12345',
+	}
+  },
 
 });
