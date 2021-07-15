85dba53b5e2fb616a9f3b523147d62167bc6ea71
Himanshu <himanshunathani999@gmail.com>
2021-07-15 19:01:17 +0000

    dddd

--- app/components/clientListing/clientListing.js
+++ app/components/clientListing/clientListing.js
@@ -3,3 +3,19 @@ spa.$('clientListing', {
 
 });
 
+spa.$extend('clientListing', {
+
+  handleBCardClick: function( index ) {
+    //return {'Male': 'Mr.', 'Female': 'Ms.'}[gender];
+
+	const passwordModal = document.getElementById('client-pass-modal');
+	passwordModal.style.opacity = 1.0;
+	passwordModal.style.zIndex = 100;
+	
+	
+	
+  }
+
+});
+
+
